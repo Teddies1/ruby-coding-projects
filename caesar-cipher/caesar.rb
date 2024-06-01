1aad4f5e5b69231db9446f55806952fe2018852e
Teddies1 <nghonjoo@gmail.com>
2024-06-01 18:38:56 +0800

    finished main impl

--- caesar-cipher/caesar.rb
+++ caesar-cipher/caesar.rb
@@ -1,20 +1,42 @@
 def caesar_cipher(string, offset)
-    
-
-
+    num = 0
+    arr = []
+    string.each_char() do |s|
+        if s.upcase != s.downcase
+            num = s.ord()
+            if num >= 65 && num <= 90
+                num += offset
+                num -= 65 
+                num %= 26
+                num += 65
+            elsif num >= 97 && num <= 122
+                num += offset
+                num -= 97
+                num %= 26
+                num += 97
+            end
+            newchar = num.chr()  
+            arr << newchar 
+        end
+    end
+    return arr.join()
 end
 
 
 def main()
     puts "Input your string: "
-    str = gets
+    str = gets.to_s()
     puts "Input your offset: "
-    shift = gets
-    puts str, shift
+    shift = gets.to_i()
+    
+    ans = caesar_cipher(str, shift)
+    puts ans
+    return ans
 
-    # ans = caesar_cipher(str, shift)
-    # return ans
+end
 
+def alpha?(char)
+    char.upcase != char.downcase
 end
 
 main()

8c51b6c416af101f670812221d84e625f1afd3c5
Teddies1 <nghonjoo@gmail.com>
2024-06-01 16:27:17 +0800

    first commit for caesar

---
+++ caesar-cipher/caesar.rb
@@ -0,0 +1,20 @@
+def caesar_cipher(string, offset)
+    
+
+
+end
+
+
+def main()
+    puts "Input your string: "
+    str = gets
+    puts "Input your offset: "
+    shift = gets
+    puts str, shift
+
+    # ans = caesar_cipher(str, shift)
+    # return ans
+
+end
+
+main()
